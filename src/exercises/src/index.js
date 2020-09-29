15cf590ce3f1ae2ff0bee51188929ef8c1fd75d5
aqrojo <aqrojo@oneclick.es>
2020-09-29 13:16:06 -0300

    ...

--- src/exercises/src/index.js
+++ src/exercises/src/index.js
@@ -1,5 +1,14 @@
+import React from 'react';
+/*
 import MultipleChoice from './multiple-choice/question/multiple-choice-question';
 
 export default {
   MultipleChoice
+}
+*/
+
+export default function Lemonade ({ data }) {
+  return(
+    <p>react components, {data.steam}</p>
+  )
 }
